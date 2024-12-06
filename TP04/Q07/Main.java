51c7e30aa3dcf2818b83e4ede4edf0a960320a83
Gabriel Vianna <gabrielvianna495@gmail.com>
2024-12-06 09:52:51 -0300

    Q07 - Hash indireto com lista encadeada

---
+++ TP04/Q07/Main.java
@@ -0,0 +1,437 @@
+import java.util.*;
+import java.io.*;
+import java.util.stream.Collectors;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.Instant;
+import java.time.Duration;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Instant start = Instant.now();
+
+        List<Pokemon> pokedex = Pokemon.lerPokemons();
+        Hash tabela = new Hash();
+
+        boolean fim = true;
+        do{
+            String entrada = sc.nextLine();
+            
+            if(entrada.equals("FIM")){fim = false;}
+
+            else{
+                        
+                try{
+                 int id = Integer.parseInt(entrada);
+                 Pokemon inserido = pokedex.get(id - 1);
+                 tabela.inserir(inserido);
+                }catch(NumberFormatException e){e.printStackTrace();
+                }catch(Exception e){
+                    e.printStackTrace();
+                    System.out.println("Erro ao inseriar");
+                }
+            }
+        }while(fim);
+
+        fim = true;
+        do{
+            String entrada = sc.nextLine();
+
+            if(entrada.equals("FIM")){fim = false;}
+            else{
+                System.out.print("=> " + entrada +": ");
+                int pos = tabela.pesquisar(entrada);
+                 
+                if(pos == -1){
+                    System.out.println("NAO");
+                }
+                else{
+                    System.out.println("(Posicao: " + pos +") SIM");
+                }
+
+                
+            }
+
+        }while(fim);
+
+        Instant end = Instant.now();
+        Duration duration = Duration.between(start, end);
+        String matricula = "729281";
+
+        try (PrintWriter escritor = new PrintWriter(new FileWriter("matrícula_HashIndireta.txt"))) {
+            escritor.println(matricula + "\t" + duration + "\t");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+       
+sc.close(); }
+}
+
+class Pokemon {
+    private int id;
+    private int generation;
+    private String name;
+    private String description;
+    private List<String> types;
+    private List<String> abilities;
+    private double weight;
+    private double height;
+    private int captureRate;
+    private boolean isLegendary;
+    private LocalDate captureDate;
+
+    // Implementação dos métodos gets e sets de todos os atributos
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setGeneration(int generation) {
+        this.generation = generation;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setType(String type) {
+        types.add(type);
+    }
+
+    public void setAbility(String ability) {
+        abilities.add(ability);
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    
+    public void setCaptureRate(int captureRate) {
+        this.captureRate = captureRate;
+    }
+    
+    public void setIsLegendary(boolean legendary) {
+        this.isLegendary = legendary;
+    }
+
+    public void setCaptureDate(LocalDate captureDate) {
+        this.captureDate = captureDate;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public int getGeneration() {
+        return this.generation;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public List<String> getTypes() {
+        return types;
+    }
+
+    public List<String> getAbilities() {
+        return abilities;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public int getCaptureRate() {
+        return this.captureRate;
+    }
+
+    public boolean getIsLegendary() {
+        return this.isLegendary;
+    }
+
+    public LocalDate getCaptureDate() {
+        return this.captureDate;
+    }
+
+    // Implementação dos construtores
+    public Pokemon() {
+        this.id = 0;
+        this.name = "";
+        this.generation = 1;
+        this.description = "";
+        this.weight = 0;
+        this.height = 0;
+        this.captureRate = 0;
+        this.isLegendary = false;
+        this.types = new ArrayList<>();
+        this.abilities = new ArrayList<>();
+    }
+    
+    public Pokemon(int id, String name, int generation, String description, List<String> types, List<String> abilities, double weight, double height, boolean isLegendary, int captureRate, LocalDate captureDate) {
+        this.id = id;
+        this.name = name;
+        this.generation = generation;
+        this.description = description;
+        this.types = types;
+        this.abilities = abilities;
+        this.weight = weight;
+        this.height = height;
+        this.isLegendary = isLegendary;
+        this.captureRate = captureRate;
+        this.captureDate = captureDate;
+    }
+
+    // Implementação do método para clonar um Pokémon
+    public static Pokemon clonePokemon(Pokemon p) {
+        return new Pokemon(p.getId(), p.getName(), p.getGeneration(), p.getDescription(), p.getTypes(), p.getAbilities(), p.getWeight(), p.getHeight(), p.getIsLegendary(), p.getCaptureRate(), p.getCaptureDate());
+    }
+
+    // Implementação do código para ler os Pokémons do arquivo CSV
+    public static List<Pokemon> lerPokemons() {
+        List<Pokemon> pokedex = new ArrayList<>();
+
+        try (BufferedReader leitor = new BufferedReader(new FileReader("pokemon.csv"))) {
+            String linha = leitor.readLine();
+            
+            while ((linha = leitor.readLine()) != null) {
+                String[] atributos = linha.split(",");
+                int pointerInicio = 0;
+                int pointerFim = 0;
+
+                for (int i = 0; i < atributos.length; i++) {
+                    for (int j = 0; j < atributos[i].length(); j++) {
+                        if (atributos[i].charAt(j) == '[') {
+                            pointerInicio = i;
+                        } else if (atributos[i].charAt(j) == ']') {
+                            pointerFim = i;
+                        }
+                    }
+                }
+                // Tira os colchetes, vírgulas, aspas duplas e simples
+                linha = linha.replaceAll("[\"'\\[\\]]", "").trim();
+                atributos = linha.split(",");
+
+                Pokemon p = new Pokemon();
+                int cont = 0;        
+                try {
+                    p.setId(Integer.parseInt(atributos[cont].trim()));
+                    cont++;
+                    p.setGeneration(Integer.parseInt(atributos[cont].trim()));
+                    cont++;
+                    p.setName(atributos[cont].trim());
+                    cont++;
+                    p.setDescription(atributos[cont].trim());
+                    cont++;
+                    p.setType(atributos[cont].trim());
+                    cont++;
+                    
+                    // Checar se da erro
+                    if (!atributos[cont].isEmpty()) {
+                        p.setType(atributos[cont].trim());
+                    }
+                    cont++;
+
+                    int qtdAbilites = (pointerFim - pointerInicio) + 1;
+
+                    for (int i = 6; i <= pointerFim; i++) {
+                        p.setAbility(atributos[i].trim());
+                    }
+                    
+                    cont += qtdAbilites;
+                    
+                    if (!(atributos[cont].isEmpty())) {
+                        p.setWeight(Double.parseDouble(atributos[cont].trim()));
+                        cont++;    
+                    } else {
+                        p.setWeight(0);
+                        cont++;
+                    }
+
+                    if (!(atributos[cont].isEmpty())) {
+                        p.setHeight(Double.parseDouble(atributos[cont].trim()));
+                        cont++;
+                    } else {
+                        p.setHeight(0);
+                        cont++;
+                    }
+
+                    if (!(atributos[cont].isEmpty())) {
+                        p.setCaptureRate(Integer.parseInt(atributos[cont].trim()));
+                        cont++;
+                    } else {
+                        p.setCaptureRate(0);
+                        cont++;
+                    }
+
+                    if (atributos[cont].equals("0")) {
+                        p.setIsLegendary(false);
+                        cont++;
+                    } else {
+                        p.setIsLegendary(true);
+                        cont++;
+                    }
+                  
+                    if (!(atributos[cont].isEmpty())) {
+                        LocalDate data = Pokemon.parseDate(atributos[cont].trim());
+                        p.setCaptureDate(data);
+                    }
+
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+
+                pokedex.add(p);
+            }
+        
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return pokedex;
+    }
+
+    private static LocalDate parseDate(String dateStr) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return LocalDate.parse(dateStr, formatter);
+    }
+    
+    public static void imprimePokemon(Pokemon p){
+        
+        String formattedDate = "Nulo";
+
+    if (p.getCaptureDate() != null) {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            formattedDate = p.getCaptureDate().format(dateFormatter);
+        }
+
+     String tiposFormatados = p.getTypes().stream().map(tipo -> "'" + tipo + "'")  // Colocando aspas simples em volta de cada tipo
+     .collect(Collectors.joining(", "));  // Juntando os tipos separados por vírgula
+
+     String abilitiesFormatadas = p.getAbilities().stream()
+    .map(ability -> "'" + ability + "'")  // Colocando aspas simples em volta de cada habilidade
+    .collect(Collectors.joining(", "));  // Juntando as habilidades separadas por vírgula
+
+
+     System.out.print("[#" + p.getId() + " -> " + p.getName() +": " + p.getDescription() + " - " + "[" + tiposFormatados + "]" + " - " + "[" + abilitiesFormatadas + "]" + " - " + p.getWeight() + "kg" + " - " + p.getHeight() + "m" + " - " +  p.getCaptureRate() + "%" + " - ");
+     System.out.println(p.getIsLegendary() + " - " + p.getGeneration() + " gen" + "]" + " - " + formattedDate);
+    
+    }
+
+}
+ class Hash {
+    Lista tabela[];
+    int tamanho;
+    final int NULO = -1;
+ 
+    public Hash() {
+       this(21);
+    }
+ 
+    public Hash(int tamanho) {
+       this.tamanho = tamanho;
+       tabela = new Lista[tamanho];
+       for (int i = 0; i < tamanho; i++) {
+          tabela[i] = new Lista();
+       }
+    }
+ 
+    public int hash(String palavra) {
+       int soma = 0;
+
+       for(int i = 0; i < palavra.length(); i++){
+        soma += (int) palavra.charAt(i);
+       }
+       return soma % tamanho;
+       }
+ 
+    public int pesquisar(String palavra) {
+       int pos = hash(palavra);
+       boolean achou = tabela[pos].pesquisar(palavra);
+
+       return achou ? pos : -1;
+    }
+ 
+    public void inserir(Pokemon elemento) {
+       int pos = hash(elemento.getName());
+       tabela[pos].inserirFim(elemento);
+    }
+ 
+
+class Celula {
+	public Pokemon elemento; // Elemento inserido na celula.
+	public Celula prox; // Aponta a celula prox.
+    
+
+    Celula(){
+    this.prox = null;
+    this.elemento = new Pokemon();
+    }
+
+	Celula(Pokemon elemento) {
+		this.elemento = Pokemon.clonePokemon(elemento);
+		this.prox = null;
+	}
+
+	Celula(Pokemon elemento, Celula prox) {
+		this.elemento = Pokemon.clonePokemon(elemento);
+		this.prox = prox;
+	}
+}
+
+ class Lista {
+	private Celula primeiro; // Primeira celula: SEM elemento valido.
+	private Celula ultimo; // Ultima celula: COM elemento valido.
+
+	
+	public Lista() {
+		primeiro = new Celula();
+		ultimo = primeiro;
+	}
+
+	
+	public void mostrar() {
+		System.out.print("[ "); // Comeca a mostrar.
+		for (Celula i = primeiro.prox; i != null; i = i.prox) {
+			System.out.print(i.elemento + " ");
+		}
+		System.out.println("] "); // Termina de mostrar.
+	}
+
+	public boolean pesquisar(String x) {
+		boolean retorno = false;
+		for (Celula i = primeiro.prox; i != null; i = i.prox) {
+         if(i.elemento.getName().equals(x)){
+            retorno = true;
+            i = ultimo;
+         }
+		}
+		return retorno;
+	}
+
+	void inserirFim(Pokemon elemento) {
+		Celula tmp = new Celula(elemento);
+		ultimo.prox = tmp;
+		ultimo = ultimo.prox;
+      tmp = null;
+	}
+	
+ }
+ }
